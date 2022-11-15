df3b2b3c810752ae4b849189a2cf045c45060f8e
pdinkins <parkerdinkins@gmail.com>
2022-11-15 17:21:55 -0500

    0.1.65

--- fnr.py
+++ fnr.py
@@ -23,52 +23,51 @@ def find_replace_json(path, find, replace):
 
 def replace_state_menus():
     old_state_menu = """
-                        <div class="card mb-4">
-                            <div class="card-header">Categories</div>
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <ul class="list-unstyled mb-0">
-                                            <li><a href="./index.html">State Sources</a></li>
-                                            <li><a href="#!">US National Sources</a></li>
-                                            <li><a href="#!">QGIS</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <ul class="list-unstyled mb-0">
-                                            <li><a href="#!">Resources</a></li>
-                                            <li><a href="#!">Tutorials</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="../index.html">Open Source GIS Data</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="../index.html">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../about.html">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../contact.html">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     """
     new_state_menu = """
-                        <div class="card mb-4">
-                            <div class="card-header">Categories</div>
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <ul class="list-unstyled mb-0">
-                                            <li><a href="./index.html">US Sources</a></li>
-                                            <li><a href="../us-federal/index.html">US Federal Sources</a></li>
-                                            <li><a href="../international/index.html">International</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <ul class="list-unstyled mb-0">
-                                            <li><a href="../software/index.html">Software</a></li>
-                                            <li><a href="../resources/index.html">Resources</a></li>
-                                            <li><a href="../tutorials/index.html">Tutorials</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="../index.html">Open Source GIS Data</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="../index.html">Home</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              Categories
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="../state/index.html">US Sources</a></li>
+                                <li><a class="dropdown-item" href="../us-federal/index.html">US Federal Sources</a></li>
+                                <li><a class="dropdown-item" href="../international/index.html">International</a></li>
+                                <li><a class="dropdown-item" href="../software/index.html">Software</a></li>
+                                <li><a class="dropdown-item" href="../resources/index.html">Resources</a></li>
+                                <li><a class="dropdown-item" href="../tutorials/index.html">Tutorials</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item"><a class="nav-link" href="../about.html">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../contact.html">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     """
 
     find_replace("C:/Users/Admin/Documents/GitHub/opensourcegisdata/v1/state", old_state_menu, new_state_menu)
+    find_replace("C:/Users/Admin/Documents/GitHub/opensourcegisdata/v1/us-federal", old_state_menu, new_state_menu)
 
 def link_update():
     find_replace("C:/Users/Admin/Documents/GitHub/opensourcegisdata/v1", '="http', '="https')
